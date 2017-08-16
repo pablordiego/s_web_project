74fdcad58ff59f3e602a474063eaa805dd2df44d
71100040 <71100040@OMEGAB08>
2017-08-16 20:04:42 -0300

    Adicionar arquivos de projeto.

---
+++ WebTestProject/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTestProject.Models
+{
+    public class Person
+    {
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Old { get; set; }
+    }
+}
